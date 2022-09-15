cc87caa47165a355ca66cf63529dd8b6d8992923
Jinyu.Guo3 <gg.jin.yu@gmail.com>
2022-09-15 16:10:49 +0800

    初步测试页面

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,58 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="app">
+    <img alt="Vue Logo" src="./assets/logo.png" />
+    <h1>
+      {{ count }}
+    </h1>
+    <h1>
+      {{ double }}
+    </h1>
+    <ul>
+      <li v-for="number in numbers" :key="number">
+        <h1>{{ number }}</h1>
+      </li>
+    </ul>
+    <h1>{{ person.name }}</h1>
+    <button @click="increase">👍+1</button>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { computed, reactive, toRefs } from "vue";
+interface DataProps {
+  count: number;
+  double: number;
+  increase: () => void;
+  numbers: number[];
+  person: { name?: string };
+}
+export default {
+  name: "App",
+  setup() {
+    // const count = ref(0);
+    // const double = computed(() => {
+    //   return count.value * 2;
+    // });
+    // const increase = () => {
+    //   count.value++;
+    // };
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
+    const data: DataProps = reactive({
+      count: 0,
+      increase: () => {
+        data.count++;
+      },
+      double: computed(() => data.count * 2),
+      numbers: [1, 2, 3],
+      person: {},
+    });
+    data.numbers[0] = 5;
+    data.person.name = "gg";
+    const toRefData = toRefs(data);
+    return { ...toRefData };
+  },
+};
 </script>
-
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
